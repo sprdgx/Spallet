bfea72cb445af0cf63af3b67a80d2db0d278aaae
sprdgx <96926256+sprdgx@users.noreply.github.com>
2024-03-06 12:03:11 +0100

    Update Home.js

--- ExtFrontend/src/components/Home.js
+++ ExtFrontend/src/components/Home.js
@@ -1,14 +1,4 @@
-import React from "react";
-import { Button } from "antd";
-import { useNavigate } from "react-router-dom";
-
-
-function Home() {
-  const navigate = useNavigate();
-  return (
-    <>
-      <div className="content">
-      import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 const App = () => {
@@ -146,33 +136,3 @@ const App = () => {
 };
 
 export default App;
-
-      <img src='https://raw.githubusercontent.com/sprdgx/SprD/main/PROJECTS/SPALLET.png' alt="logo" className="frontPageLogo" />
-      <h2> Hey There 👋 </h2>
-        <h4 className="h4"> Welcome to your Web3 Wallet</h4>
-        <Button
-          onClick={() => navigate("/createwallet")}        
-          className="frontPageButton"
-          type="primary"
-        >
-          Create A Wallet
-        </Button>
-        <Button
-          onClick={() => navigate("/recoverwallet")}        
-          className="frontPageButton"
-          type="default"
-        >
-          Sign In With Seed Phrase
-        </Button>
-        <p className="frontPageBottom">
-          Find Alt Coin Gems:{" "}
-          <a href="https://moralismoney.com/" target="_blank" rel="noreferrer">
-            money.moralis.io
-          </a>
-        </p>
-      </div>
-    </>
-  );
-}
-
-export default Home;
